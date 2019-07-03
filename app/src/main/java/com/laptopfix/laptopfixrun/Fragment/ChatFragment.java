cc9bd36cf9d2c97dc7b9b8aec9e3266f327f52c0
OsvaldoEB <osvaldo.jaesba@hotmail.com>
2019-07-03 10:21:18 -0500

    Chat por parte del usuario Laptopfix

--- app/src/main/java/com/laptopfix/laptopfixrun/Fragment/ChatFragment.java
+++ app/src/main/java/com/laptopfix/laptopfixrun/Fragment/ChatFragment.java
@@ -9,9 +9,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -19,6 +22,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import com.laptopfix.laptopfixrun.Adapter.CustomerAdapter;
@@ -38,6 +42,8 @@ public class ChatFragment extends Fragment {
     private RecyclerView recyclerView;
     private CustomerAdapter customerAdapter;
     private List<Customer> mCustomer;
+    EditText search_customer;
+
     private AlertDialog dialog;
 
 
@@ -55,8 +61,58 @@ public class ChatFragment extends Fragment {
         mCustomer = new ArrayList<>();
 
         readCustomer();
+        search_customer = view.findViewById(R.id.search_customers);
+        search_customer.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchCustomer(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         return view;
+    }
+
+    private void searchCustomer(String toString) {
+        final FirebaseUser fuser= FirebaseAuth.getInstance().getCurrentUser();
+        Query query = FirebaseDatabase.getInstance().getReference(Common.CUSTOMER_TABLE).orderByChild("name")
+                .startAt(toString)
+                .endAt(toString+"\uf8ff");
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                    mCustomer.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        Customer customer = snapshot.getValue(Customer.class);
+
+                        assert customer != null;
+                        assert fuser != null;
+                        if (!customer.getIdCus().equals(fuser.getUid())) {
+                            mCustomer.add(customer);
+                        }
+                    }
+
+                    customerAdapter = new CustomerAdapter(getContext(), mCustomer);
+                    recyclerView.setAdapter(customerAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -73,24 +129,25 @@ public class ChatFragment extends Fragment {
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                mCustomer.clear();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    Customer customer = snapshot.getValue(Customer.class);
+                if (search_customer.getText().toString().equals("")) {
+                    mCustomer.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        Customer customer = snapshot.getValue(Customer.class);
 
-                    assert customer != null;
-                    assert firebaseUser != null;
-                    mCustomer.add(customer);
-                    /*if (!customer.getIdCus().equals(firebaseUser.getUid())){
+                        assert customer != null;
+                        assert firebaseUser != null;
+                        //mCustomer.add(customer);
+                    if (!customer.getIdCus().equals(firebaseUser.getUid())){
                         mCustomer.add(customer);
-                    }*/
+                    }
 
 
+                    }
 
+                    customerAdapter = new CustomerAdapter(getContext(), mCustomer);
+                    recyclerView.setAdapter(customerAdapter);
+                    dialog.dismiss();
                 }
-
-                customerAdapter = new CustomerAdapter(getContext(), mCustomer);
-                recyclerView.setAdapter(customerAdapter);
-                dialog.dismiss();
             }
 
             @Override
